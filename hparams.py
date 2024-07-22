aedca28aa92337cc4341795f37f6053ca7ffb759
DongGyu Kim <57940897+kimdonggyu2008@users.noreply.github.com>
2024-07-22 22:22:30 +0900

    Add files via upload

--- hparams.py
+++ hparams.py
@@ -15,7 +15,7 @@ def create_hparams(hparams_string=None, verbose=False):
         # Experiment Parameters        #
         ################################
         "epochs":1500,
-        "iters_per_checkpoint":1000,
+        "iters_per_checkpoint":100,
         "seed":1234,
         "dynamic_loss_scaling":True,
         "fp16_run":False,
